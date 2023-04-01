f14eb3a4844fae290cc178459b14bb591d02b892
cuiti <1478158437@qq.com>
2023-04-01 19:53:42 +0800

    update_test_file

---
+++ 24 how long the word is.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+string s,temp;
+int a[305];
+int main(){
+	int i=0;
+	while(cin>>temp){
+		a[i]=temp.size();
+		i++;
+	}
+	cout<<a[0];
+	for(int i=1;i<300;i++){
+		if(a[i]) cout<<','<<a[i];
+	}
+	return 0;
+}
+
